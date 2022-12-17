5ffaa683f740475db97806296b3735a11941e9f6
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-17 10:16:19 -0500

    Import Person.js
    To render progress

--- cv-project/src/components/cv-page/Page.js
+++ cv-project/src/components/cv-page/Page.js
@@ -1,9 +1,10 @@
+import Person from '../Person/Person';
 import './Page.css';
 
 function Page () {
     return (
         <div className='page-container'>
-
+            <Person />
         </div>
     );
 };
