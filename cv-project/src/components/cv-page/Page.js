5ab6c034a2c193dc52ccd010935e3d81740e0c8c
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-17 13:42:31 -0500

    Import Person components modal with form

--- cv-project/src/components/cv-page/Page.js
+++ cv-project/src/components/cv-page/Page.js
@@ -3,6 +3,7 @@ import Education from '../Education/Education';
 import Footer from '../Footer/Footer';
 import Leadership from '../Leadership/Leadership';
 import Person from '../Person/Person';
+import PersonModal from '../Person/PersonModal/PersonModal';
 import Professional from '../Professional/Professional';
 import Relevant from '../Relevant/Relevant';
 import Skills from '../Skills/Skills';
@@ -19,6 +20,7 @@ function Page () {
             <Leadership />
             <Contact />
             <Footer />
+            <PersonModal />
         </div>
     );
 };
