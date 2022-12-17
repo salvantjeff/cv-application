c1453a2db5f303440c9d440f72bab432d1754ea0
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-17 10:41:24 -0500

    Import required images and css files

---
+++ cv-project/src/components/Professional/Professional.js
@@ -0,0 +1,5 @@
+import './Professional.css';
+import ngLogo from '../../img/ng-logo.png';
+import neofectLogo from '../../img/neofect-logo.jpeg';
+import editPencil from '../../img/pencil.png';
+import addSymbol from '../../img/add-1.png';
