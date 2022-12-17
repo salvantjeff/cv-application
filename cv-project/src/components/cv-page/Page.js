423c0892d603e76fb354200e2cc4c64169912089
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-17 13:59:46 -0500

    Remove the PersonModal.js import

--- cv-project/src/components/cv-page/Page.js
+++ cv-project/src/components/cv-page/Page.js
@@ -3,7 +3,6 @@ import Education from '../Education/Education';
 import Footer from '../Footer/Footer';
 import Leadership from '../Leadership/Leadership';
 import Person from '../Person/Person';
-import PersonModal from '../Person/PersonModal/PersonModal';
 import Professional from '../Professional/Professional';
 import Relevant from '../Relevant/Relevant';
 import Skills from '../Skills/Skills';
@@ -20,7 +19,7 @@ function Page () {
             <Leadership />
             <Contact />
             <Footer />
-            <PersonModal />
+            
         </div>
     );
 };
