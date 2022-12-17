272335b21b19108ad0b5bf942d7e9c1ae2aebd25
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-17 10:52:23 -0500

    Import required images and css files

---
+++ cv-project/src/components/Contact/Contact.js
@@ -0,0 +1,5 @@
+import './Contact.css';
+import emailLogo from '../../img/mail.png';
+import phoneLogo from '../../img/phones.png';
+import linkedInLogo from '../../img/linkedin-logo.png';
+import editPencil from '../../img/pencil.png';
