5cf7ee61988bdbd702480b169bb52f546ecb1eb7
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-17 11:22:07 -0500

    Create minimum required state for Person.js
    Used the state to replace static content

--- cv-project/src/components/Person/Person.js
+++ cv-project/src/components/Person/Person.js
@@ -1,27 +1,35 @@
+import React, { useState } from 'react';
 import './Person.css';
 import profileImg from '../../img/messi.jpeg';
 import editPencil from '../../img/pencil.png';
 
 function Person () {
+    const [person, setPerson] = useState({
+        name: 'Jeff Salvant',
+        position: 'Student',
+        img: profileImg,
+        summary: `Virginia Commonwealth university graduate. Currently, studying software 
+            engineering to contribute in the tech industry.
+            ldkjflakfjasdlfkjasdlfkjsdlfkasdjflksdjflasdkjfldkfjaslfkjdlfkjasdflkj`,
+    });
+
     return (
         <div className='person'>
             <div className="section-heading">
-                <h1 className="person-name">Jeff Salvant</h1>
+                <h1 className="person-name">{person.name}</h1>
                 <div>
                     <img className="edit-section" src={editPencil} alt="edit section"/>
                 </div>
             </div>
             <div className="person-details">
                 <div className='person-info'>
-                    <p className="person-position">Student</p>
+                    <p className="person-position">{person.position}</p>
                     <p className="person-summary">
-                        Virginia Commonwealth university graduate. Currently, studying software 
-                        engineering to contribute in the tech industry.
-                        ldkjflakfjasdlfkjasdlfkjsdlfkasdjflksdjflasdkjfldkfjaslfkjdlfkjasdflkj
+                        {person.summary}
                     </p>
                 </div>
                 <div className="person-image-box">
-                    <img className="person-image" src={profileImg} alt="profile pic"/>
+                    <img className="person-image" src={person.img} alt="profile pic"/>
                 </div>
             </div>
         </div>
