bcf0af53c9b1540da1a74b11deae25ae0c992e18
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-17 10:50:18 -0500

    Import Leadership.js into Page.js
    To be rendered

--- cv-project/src/components/cv-page/Page.js
+++ cv-project/src/components/cv-page/Page.js
@@ -1,4 +1,5 @@
 import Education from '../Education/Education';
+import Leadership from '../Leadership/Leadership';
 import Person from '../Person/Person';
 import Professional from '../Professional/Professional';
 import Relevant from '../Relevant/Relevant';
@@ -13,6 +14,7 @@ function Page () {
             <Skills />
             <Professional />
             <Relevant />
+            <Leadership />
         </div>
     );
 };
