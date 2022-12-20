969a2446c8366b42a400813104613e1c191038cb
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-20 10:43:26 -0500

    Render each professional card from the array

--- cv-project/src/components/Professional/Professional.js
+++ cv-project/src/components/Professional/Professional.js
@@ -5,8 +5,43 @@ import editPencil from '../../img/pencil.png';
 import addSymbol from '../../img/add-1.png';
 import React, { useState, useEffect } from 'react';
 import ProfessionalModal from './ProfessionalModal/ProfessionalModal';
+import { v4 as uuidv4 } from 'uuid';
 
 function Professional () {
+    const initProfessionals = [
+        {
+            position: 'Structural Design Engineer Intern',
+            company: 'Northrop Grumman',
+            startDate: 'May 2021',
+            endDate: 'Aug 2021',
+            location: 'Palmdale, California',
+            summary: `Contributed to the structural design team by reviewing and fixing drawing designs`,
+            id: uuidv4(),
+        },
+        {
+            position: 'Customer support Intern',
+            company: 'Neofect',
+            startDate: 'June 2019',
+            endDate: 'Aug 2019',
+            location: 'Richmond, Virginia',
+            summary: `
+                flsdkafjlasdkfjl;akfjldkfjaslkfjdl;kfjsdalkjflkasdjf
+                lsdakfjlasd;kfjsdalfkjslakfjsdlkfjasdlkfjslakfjasdlkfjasdlkfjsdlkf
+                jaslkfjl;sakfjsdal;fkj;sdlfkjdaslf;ksdjflkasjdflkdsjflasdkjflasd
+                kfjlsdkfjlkfjal;kfjasdl;fkjsdlofksjflaskfjldkfjasdl;fkjdl;askjfas
+                ifjffoijfkldsfjaklfjdklfjaldjflo;aifjdsl;ifjawofiejlofasdjnvlsdf
+                jkgjoigjgpoiawgjaopigjerpgiorjgtoiaerghjoighjeoaighjaergtyiojrgkl
+                sdfgjnlkfghjadefpioghjopigjdfigjskgjsgkljfkjfkfaklfjslakfjalkfks
+                jflksajflksdfjokjlkjlfkjsldakfjlksdjfklsjfslakfjasl;dfjo2itjugo
+                uhjoighjoighjeraoguihag
+            `,
+            id: uuidv4(),
+        }
+    ];
+    const [index, setIndex] = useState(0);
+    const [professionals, setProfessionals] = useState(initProfessionals);
+    const [professionalsInfo, setProfessionalsInfo] = useState(initProfessionals);
+
     const [modal, setModal] = useState(false);
     useEffect(() => {
         if (modal) {
@@ -26,6 +61,7 @@ function Professional () {
         // setIndex(newIndex);
         toggleModal();
     }
+    console.log(professionalsInfo);
     return(
         <div className='professional'>
             <div className="heading-block">
@@ -38,59 +74,34 @@ function Professional () {
                 </div>
             </div>
             <div className='all-professional-exp'>
-                <div className='professional-exp'>
-                    <div className='professional-icon-box'>
-                        <img className="professional-icon" src={ngLogo} alt="professional icon"/>
-                    </div>
-                    <div className='professional-exp__details'>
-                        <div className="section-card">
-                            <p className='position'>Structural Design Engineer Intern</p>
-                            <div className="edit-section-box">
-                                <img 
-                                className="edit-section" 
-                                src={editPencil} 
-                                alt="edit section"
-                                onClick={handleEditClicked}
-                                />
+                {professionalsInfo.map((professional, index) => {
+                    return (
+                        <div key={professional.id} className='professional-exp'>
+                            <div className='professional-icon-box'>
+                                <img className="professional-icon" src={ngLogo} alt="professional icon"/>
+                            </div>
+                            <div className='professional-exp__details'>
+                                <div className="section-card">
+                                    <p className='position'>{professional.position}</p>
+                                    <div className="edit-section-box">
+                                        <img 
+                                        className="edit-section" 
+                                        src={editPencil} 
+                                        alt="edit section"
+                                        onClick={handleEditClicked}
+                                        />
+                                    </div>
+                                </div>
+                                <p className='company'>{professional.company}</p>
+                                <p className='time-worked'>{professional.startDate} - {professional.endDate}</p>
+                                <p className='work-location'>{professional.location}</p>
+                                <p className='work-summary'>
+                                    {professional.summary}
+                                </p>
                             </div>
                         </div>
-                        <p className='company'>Northrop Grumman</p>
-                        <p className='time-worked'>May 2021 - Aug 2021</p>
-                        <p className='work-location'>Palmdale, California</p>
-                        <p className='work-summary'>
-                            Contributed to the structural design team by 
-                            reviewing and fixing drawing designs
-                        </p>
-                    </div>
-                </div>
-
-                <div className='professional-exp'>
-                    <div className='professional-icon-box'>
-                        <img className="professional-icon" src={neofectLogo} alt="professional icon"/>
-                    </div>
-                    <div className='professional-exp__details'>
-                        <div className="section-card">
-                            <p className='position'>Structural Design Engineer Intern</p>
-                            <div className="edit-section-box">
-                                <img className="edit-section" src={editPencil} alt="edit section"/>
-                            </div>
-                        </div>                        <p className='company'>Northrop Grumman</p>
-                        <p className='time-worked'>May 2021 - Aug 2021</p>
-                        <p className='work-location'>Palmdale, California</p>
-                        <p className='work-summary'>
-                            Contributed to the structural design team by reviewing and fixing 
-                            drawing designs flsdkafjlasdkfjl;akfjldkfjaslkfjdl;kfjsdalkjflkasdjf
-                            lsdakfjlasd;kfjsdalfkjslakfjsdlkfjasdlkfjslakfjasdlkfjasdlkfjsdlkf
-                            jaslkfjl;sakfjsdal;fkj;sdlfkjdaslf;ksdjflkasjdflkdsjflasdkjflasd
-                            kfjlsdkfjlkfjal;kfjasdl;fkjsdlofksjflaskfjldkfjasdl;fkjdl;askjfas
-                            ifjffoijfkldsfjaklfjdklfjaldjflo;aifjdsl;ifjawofiejlofasdjnvlsdf
-                            jkgjoigjgpoiawgjaopigjerpgiorjgtoiaerghjoighjeoaighjaergtyiojrgkl
-                            sdfgjnlkfghjadefpioghjopigjdfigjskgjsgkljfkjfkfaklfjslakfjalkfks
-                            jflksajflksdfjokjlkjlfkjsldakfjlksdjfklsjfslakfjasl;dfjo2itjugo
-                            uhjoighjoighjeraoguihag
-                        </p>
-                    </div>
-                </div>
+                    )
+                })}
             </div>
             <ProfessionalModal 
                 toggleModal={toggleModal}
