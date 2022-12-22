6495d10fe606368a154256f54e0728fb683dc1ec
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-22 12:16:14 -0500

    Add imageURL as prop in professionals state obj

--- cv-project/src/components/Professional/Professional.js
+++ cv-project/src/components/Professional/Professional.js
@@ -16,6 +16,7 @@ function Professional () {
             endDate: '2021-08',
             location: 'Palmdale, California',
             summary: `Contributed to the structural design team by reviewing and fixing drawing designs`,
+            imageURL: '',
             id: uuidv4(),
         },
         {
@@ -25,6 +26,7 @@ function Professional () {
             endDate: '2019-08',
             location: 'Richmond, Virginia',
             summary: 'Helped resolve angry/upset/confused/kind customers issues.',
+            imageURL: '',
             id: uuidv4(),
         }
     ];
@@ -39,6 +41,7 @@ function Professional () {
         endDate: '',
         location: '',
         summary: '',
+        imageURL: '',
         id: uuidv4(),
     });
 
@@ -121,6 +124,7 @@ function Professional () {
             endDate: '',
             location: '',
             summary: '',
+            imageURL: '',
             id: uuidv4(),
         });
         console.log('UPDATE COMPLETE');
