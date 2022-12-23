80f66e6b787586ab1d43b5602829ec1ed8ff5509
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-22 22:54:02 -0500

    Create basic header component

---
+++ cv-project/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="app-header">
+            <h1>Welcome</h1>
+        </header>
+    );
+};
+
+export default Header;
