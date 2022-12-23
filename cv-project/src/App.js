c230d46b5469ddac4e14bd2740c4387af661af56
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-22 22:56:38 -0500

    Import header into app component

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Page from './components/cv-page/Page';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Page />
     </div>
   );
